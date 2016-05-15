a43eeba9e660bc28fcc3ecff73960ca32d9a3c1a
Bob Lee <cloudhc@gmail.com>
2016-05-15 20:21:09 +0900

    Add WeatherWidget by partial feature.

--- meadowlark.js
+++ meadowlark.js
@@ -4,6 +4,40 @@ var express = require('express'),
 		.create({defaultLayout: 'main'}),
 	fortune = require('./lib/fortune.js');
 
+var tours = [
+	{id: 0, name: 'Hood River', price: 99.99},
+	{id: 1, name: 'Oregon Coast', price: 149.95},
+];
+
+// mocked weather data
+function getWeatherData(){
+    return {
+        locations: [
+	    	{
+				name: 'Portland',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)',
+		    },
+		    {
+		        name: 'Bend',
+		        forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+		        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+		        weather: 'Partly Cloudy',
+		        temp: '55.0 F (12.8 C)',
+		    },
+		    {
+		        name: 'Manzanita',
+		        forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+		        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+		        weather: 'Light Rain',
+		        temp: '55.0 F (12.8 C)',
+		    },
+        ],
+    };
+}
+
 app
 	.use(express.static(__dirname + '/public'))
 	.set('port', process.env.PORT || 3000)
@@ -14,6 +48,11 @@ app
 app
 	.use(function(req, res, next) {
 		res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+		next();
+	})
+	.use(function(req, res, next) {
+		if(!res.locals.partials) res.locals.partials = {};
+		res.locals.partials.weatherContext = getWeatherData();
 		next();
 	});
 
@@ -31,8 +70,58 @@ app
 	.get('/tours/hood-river', function(req, res) {
 		res.render('tours/hood-river');
 	})
+	.get('/tours/oregon-coast', function(req, res){
+		res.render('tours/oregon-coast');
+	})
 	.get('/tours/request-group-size', function(req, res) {
 		res.render('tours/request-group-size');
+	})
+	.get('/api/tours', function(req, res) {
+		var toursXml = '' + 
+			products.map(function(p) {
+				return ' id="' + p.id + '">' + p.name + '';
+			}).join('') + '';
+		var toursText = tours.map(function(p) {
+			return p.id + ': ' + p.name + ' (' + p.price + ')';
+		}).join('\n');
+		res.format({
+			'application/json': function() {
+				res.json(tours);
+			},
+			'applciation/xml': function() {
+				res.type('application/xml');
+				res.send(toursXml);
+			},
+			'text/xml': function() {
+				res.type('text/xml');
+				res.send(toursXml);
+			},
+			'text/plain': function() {
+				res.type('text/plain');
+				res.send(toursXml);
+			}
+		});
+	})
+	.put('/api/tours/:id', function(req, res) {
+		var p = tours.filter(function(p) { return p.id == req.param.id })[0];
+		if (p) {
+			if (req.query.name) p.name = req.query.name;
+			if (req.query.price) p.price = req.query.price;
+			res.json({success: true});
+		} else {
+			res.json({error: 'No such tour exists.'});
+		}
+	})
+	.delete('/api/tours/:id', function(req, res) {
+		var i;
+		for( var i = tours.length-1; i >= 0 ; i--)
+			if (tours[i].id == req.params.id) break;
+		if (i >= 0) {
+			tours.splice(i, 1);
+			res.json({success: true});
+		} else {
+			res.json({error: 'No such tour exists.'});
+		}
 	});
 
 app
